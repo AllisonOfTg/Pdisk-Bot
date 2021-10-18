e18fd4d8701fde50f73b1987a9661ce476cab110
[̲̅A][̲̅L][̲̅L][̲̅I][̲̅S][̲̅O][̲̅N] <73114327+AllisonOfTg@users.noreply.github.com>
2021-10-18 12:58:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,24 +51,24 @@ PDiskBot = Client(
 
 @PDiskBot.on_message(filters.command("start") & ~filters.edited)
 async def start_handler(_, m: Message):
-    await m.reply_text("Hi, I am Alive!\n\nSearch using /request command.", quote=True)
+    await m.reply_text("Hi, I am Alive!\n\nSearch using /request command\n\n©️MᴀɪɴᴛᴀɪɴᴇD Bʏ Mᴏᴠɪᴇ Cʟᴜʙ.", quote=True)
 
 
 @PDiskBot.on_message(filters.command("request", prefixes=["#", "/"]) & ~filters.edited, group=-1)
 async def text_handler(_, m: Message):
     if len(m.command) < 2:
-        return await m.reply_text("Search Query Missing!")
+        return await m.reply_text("Search Query Missing!\n\nJoin <a href=https://t.me/MOVIECLUB_CHAT>Mᴏᴠɪᴇ Cʟᴜʙ</a> For Getting Your Favourite Movies.")
     editable = await m.reply_text("Please Wait ...", quote=True)
     response = await search_pdisk_videos(m.text.split(" ", 1)[-1], Configs.PDISK_USERNAME, Configs.PDISK_PASSWORD)
     if isinstance(response, Exception):
         traceback.print_exc()
         try: await editable.edit("Failed to search!",
                                  reply_markup=InlineKeyboardMarkup([
-                                     [InlineKeyboardButton("Sumpot Group", url="https://t.me/JoinOT")]
+                                     [InlineKeyboardButton("Movie Group", url="https://t.me/MOVIECLUB_CHAT")]
                                  ]))
         except MessageNotModified: pass
     elif not response["data"]["list"]:
-        try: await editable.edit("Not Found!")
+        try: await editable.edit("Not Found!\n\nJoin <a href=https://t.me/MOVIECLUB_CHAT>Mᴏᴠɪᴇ Cʟᴜʙ</a> For Getting Your Favourite Movies.")
         except MessageNotModified: pass
     else:
         data = response["data"]["list"]
@@ -80,7 +80,7 @@ async def text_handler(_, m: Message):
             count += 1
             text += f"**Title:** `{data[i]['title']}`\n" \
                     f"**Description:** `{data[i]['description']}`\n" \
-                    f"**PDisk Link:** {Configs.PDISK_DOMAIN + 'share-video?videoid=' + data[i]['share_link'].split('=', 1)[-1]}\n\n"
+                    f"**PDisk Link:** {Configs.PDISK_DOMAIN + 'share-video?videoid=' + data[i]['share_link'].split('=', 1)[-1]}\n\n© Pᴏᴡᴇʀᴇᴅ Bʏ <a href='https://t.me/MOVIECLUB_CHAT'>Mᴏᴠɪᴇ Cʟᴜʙ</a></b>\n"
         try: await editable.edit(text, disable_web_page_preview=True)
         except MessageNotModified: pass
 
